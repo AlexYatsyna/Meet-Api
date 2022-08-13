8f4f47863744a805ef41177eaef6a292e45a4dfd
Alex Yatsyna <yatsyna.firebalde@mail.ru>
2022-08-13 17:57:13 +0300

    created solution, added layers

---
+++ MeetUp.Data/MeetupEventModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetUp.Data
+{
+    public class MeetupEventModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Topic { get; set; }
+        public string Description { get; set; }
+        public string Plan { get; set; }
+        public DateTime TimeEvent { get; set; }
+        public string Location { get; set; }
+
+        // Organizers
+        //Speakers
+    }
+}
